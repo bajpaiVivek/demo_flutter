337376c61558a722a190faee6b2a87a38b99251a
vivek_bajpai <bajpai8765vivek@gmail.com>
2023-12-26 16:35:12 +0000

    some changes

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -8,4 +8,17 @@ class Category {
     required this.name,
     required this.desc,
   });
+  Category.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    desc = json['desc'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['desc'] = this.desc;
+    return data;
+  }
 }
